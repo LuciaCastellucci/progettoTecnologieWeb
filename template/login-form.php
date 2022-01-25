f358ac232507950e3d04fc8fe5b3e2d16983f140
olivia nguemo <oliviarannick.nguemo@studio.unibo.it>
2022-01-25 13:55:36 +0100

    add login-form

---
+++ template/login-form.php
@@ -0,0 +1,20 @@
+<form action="#" method="POST">
+            <h2>Login</h2>
+            <?php if(isset($templateParams["errorelogin"])): ?>
+            <p><?php echo $templateParams["errorelogin"]; ?></p>
+            <?php endif; ?>
+            <ul>
+                <li>
+                    <label for="username">Username:</label><input type="text" id="username" name="username" />
+                </li>
+                <li>
+                    <label for="password">Password:</label><input type="password" id="password" name="password" />
+                </li>
+                <li>
+                    <label for="email">Email:</label><input type="text" id="email" name="email" />
+                </li>
+                <li>
+                    <input type="submit" name="submit" value="Invia" />
+                </li>
+            </ul>
+        </form>
