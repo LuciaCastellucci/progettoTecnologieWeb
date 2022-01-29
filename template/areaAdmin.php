b7c25da01e20917709085389e767d0c24459431e
olivia nguemo <oliviarannick.nguemo@studio.unibo.it>
2022-01-29 17:20:23 +0100

    css di areaUtente ancora da sistemare e areaAdmin

---
+++ template/areaAdmin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+<a class="navbar-brand" href="index.php"><img src=".\resources\images\logo.png" alt="logo"></a>
+  <h1>Welcome <?php echo $_GET["username"]; ?><br> </h1>
+  <a href="index.php" class="nav-link px-2 link-secondary">Aggiungi Articoli</a>
+  <a href="index.php" class="nav-link px-2 link-secondary">Home</a>
+</div>
+
+ 
+
+
+</body>
+</html>
