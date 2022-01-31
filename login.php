259b009d95416553c29b36888ebe4406c3dcfa95
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-01-31 19:58:21 +0100

    Change login.php

--- login.php
+++ login.php
@@ -14,30 +14,13 @@ if(isset($_POST["username"]) && isset($_POST["password"])){
 }
 
 if(isUserLoggedIn()){
-    echo "area riservata";
-    $admin_result = $dbh->isAdmin($_SESSION["username"]);
-    if(count($admin_result)==0){
-        $templateParams["titolo"] = "Area riservata cliente";
-        $templateParams["nome"] = "template/areaCliente.php";
-        $templateParams["css"] = "css/areaUtente.css";
-
-        echo "area riservata cliente";
-    }
-    else{
-        $templateParams["titolo"] = "Admin";
-        $templateParams["nome"] = "template/areaAdmin.php";
-        $templateParams["css"] = "css/areaUtente.css";
-        echo "area riservata admin";
-    }
-    //$templateParams["titolo"] = "Area utente";
-    //$templateParams["nome"] = "template/areaUtente.php";
-    //$templateParams["css"] = "css/areaUtente.css";
+    require 'areaUtente.php';
 }
 else{
     $templateParams["titolo"] = "Login";
     $templateParams["nome"] = "template/login-form.php";
     $templateParams["css"] = "css/login.css";
+    require 'template/base_header.php';
 }
 
-require 'template/base_header.php';
 ?>
